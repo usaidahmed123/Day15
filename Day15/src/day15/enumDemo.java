e8e1ccc62650213d4133056ce6ee95ce4cc0022e
jawad <jawadusaid11@gmail.com>
2022-02-26 08:48:03 +0530

    enum

--- Day15/src/day15/enumDemo.java
+++ Day15/src/day15/enumDemo.java
@@ -1,14 +1,40 @@
 package day15;
 
 enum Mobile {
+	
 	SAMSUNG, GTC, XIAOMI, BLACKBERRY;
+	int price=15;
+	public int getprice() {
+		return price;
+	}
 }
 
 public class enumDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Mobile.XIAOMI);
+		
+		Mobile m = Mobile.XIAOMI;
+		switch(m)
+		{
+		case GTC:
+			System.out.println("Some local company");
+			break;
+		case XIAOMI:
+			System.out.println("Best company from my point of view");
+			break;
+		case SAMSUNG:
+			System.out.println("baddest experience company from my pov");
+			break;
+		case BLACKBERRY:
+			System.out.println("baddest experience company from my pov");
+			break;
+		}
+		
+		
+		
+		
+		System.out.println(Mobile.BLACKBERRY.getprice());
 	}
 
 }

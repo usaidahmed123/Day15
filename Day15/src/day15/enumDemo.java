a1c96e554d1eca333f8d6558c4c7b4970430e9fb
jawad <jawadusaid11@gmail.com>
2022-02-26 08:33:48 +0530

    enum

---
+++ Day15/src/day15/enumDemo.java
@@ -0,0 +1,14 @@
+package day15;
+
+enum Mobile {
+	SAMSUNG, GTC, XIAOMI, BLACKBERRY;
+}
+
+public class enumDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Mobile.XIAOMI);
+	}
+
+}
